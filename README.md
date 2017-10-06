d3cc2aa8ad95b61be019cdbb3a74a4516fd04335
James Kent <jkent@thoughtworks.com>
2017-10-06 12:26:38 -0400

    feat(title regex): updates title configuration to a regex

--- README.md
+++ README.md
@@ -56,7 +56,7 @@ Name          | Type     | Default  | Required | Description
 Name       | Type      | Default | Required | Description
 ---------- | --------- | ------- | -------- | -----------
 **`meta`** | `Boolean` | `false` | `✖️`      | check page meta
-**`titleConstant`** | `String` | `TELUS.com` | `✖️`      | configurable domain name for <title> assertions
+**`titleRegExp`** | `RegExp` | `/.* - TELUS.com/` | `✖️`      | configurable regex for <title> assertions
 
 ## SEO Rules
 
@@ -83,8 +83,7 @@ This is a work in progress, checklist below indicates what has been implemented 
 
 - `<title>`
   - [x] element should exists and be unique
-  - [x] element should end with company domain name (`TELUS.com`) or `titleConstant`
-  - [x] element should use a spaced dash (` - `) to separate sections
+  - [x] element should conform to pattern (` - TELUS.com`)
   - [x] element content length should be not exceed `65` characters
 
 - `<meta name="description">`
--- assertions/seo.js
+++ assertions/seo.js
@@ -1,5 +1,7 @@
 module.exports.assertion = function (context, options) {
-  options = Object.assign({ meta: true }, options)
+  options = Object.assign({ meta: true, titleRegExp: /.* - TELUS.com/ }, options)
+
+  if (!(options.titleRegExp instanceof RegExp)) throw new TypeError('`titleRegExp` should be type RegExp')
 
   this.message = `${context} passes SEO checklist`
   this.value = (result) => result
@@ -27,9 +29,7 @@ module.exports.assertion = function (context, options) {
 
       this.api.getTitle((value) => {
         // TODO: what's the length for French & other languages?
-        const titleConstant = options.titleConstant || 'TELUS.com'
-        assert.ok(new RegExp(`${titleConstant}$`).test(value), `<title/> should end with company domain name (${titleConstant})`)
-        assert.ok(/ - /.test(value), '<title/> should use a spaced dash (` - `) to separate sections')
+        assert.ok(options.titleRegExp.test(value), `<title/> should conform to pattern (${options.titleRegExp})`)
         assert.ok(value.length <= 65, '<title/> content length should be not exceed `65` characters')
       })
 
