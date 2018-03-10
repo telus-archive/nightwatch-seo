6ae4cd0e8442b74a40163ee884e6b74130682277
Ahmad Nassri <ahmad@ahmadnassri.com>
2018-03-09 22:34:26 -0500

    fix(travis): check for html successful load before running seo assert

--- test/specs/index.js
+++ test/specs/index.js
@@ -5,6 +5,10 @@ module.exports = {
     browser
       .url(`http://localhost:${port}`)
 
+      .waitForElementVisible('h1', 3000)
+
+      .assert.containsText('h1', 'Hello World')
+
       .assert.seo('html', { meta: true })
 
       .end()
